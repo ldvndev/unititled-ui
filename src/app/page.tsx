f77782fe63fd1209e1caedea04782a605e0b4b91
ldvndev <ldvndev@gmail.com>
2024-01-10 19:14:01 -0300

    feat: Add Layout Structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
 export default function Home() {
-  return (
-    <div className="p-4">
-      <h1 className="flex items-center gap-3 text-5xl font-bold text-violet-500 before:flex before:h-8 before:w-0.5 before:bg-sky-500">
-        Home
-      </h1>
-      <button
-        disabled
-        className="mt-4 rounded-md bg-sky-500 px-4 py-2 font-medium enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60"
-      >
-        Sign in
-      </button>
-    </div>
-  )
+  return <h1>Hello world</h1>
 }
