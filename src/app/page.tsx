27da7c4635d858a9234dd6d447d7f7fe3bc36275
ldvndev <ldvndev@gmail.com>
2024-01-10 18:55:40 -0300

    feat: ESLint and Prettier configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 export default function Home() {
   return (
     <div className="p-4">
-      <h1 className="font-bold text-5xl text-violet-500">
+      <h1 className="flex items-center gap-3 text-5xl font-bold text-violet-500 before:flex before:h-8 before:w-0.5 before:bg-sky-500">
         Home
       </h1>
+      <button
+        disabled
+        className="mt-4 rounded-md bg-sky-500 px-4 py-2 font-medium enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60"
+      >
+        Sign in
+      </button>
     </div>
-  ) 
+  )
 }
