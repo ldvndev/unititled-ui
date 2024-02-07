9f7ec4c877e138eeb8d71ee686a5c7caf5c43173
ldvndev <ldvndev@gmail.com>
2024-02-06 22:29:20 -0300

    feat: Add dark theme to from and sidebar

--- src/app/components/Sidebar/Profile.tsx
+++ src/app/components/Sidebar/Profile.tsx
@@ -9,10 +9,10 @@ export function Profile() {
         className="h-10 w-10 rounded-full"
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Leonardo Rodrigues
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           ldvndev@gmail.com
         </span>
       </div>
